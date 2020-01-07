77dfcbbeb4b9808443da9ef6af7b38d9c513e6c2
Simon Koch <simon.koch@tu-braunschweig.de>
2020-01-07 15:24:21 +0100

    commented deprecated flags and increased expected version from 30 to 70

--- src/util.php
+++ src/util.php
@@ -132,18 +132,18 @@ function get_flag_info() : array {
         ],
         ast\AST_ASSIGN_OP => $sharedBinaryOps + [
             // Old version 10 flags
-            flags\ASSIGN_BITWISE_OR => 'ASSIGN_BITWISE_OR',
-            flags\ASSIGN_BITWISE_AND => 'ASSIGN_BITWISE_AND',
-            flags\ASSIGN_BITWISE_XOR => 'ASSIGN_BITWISE_XOR',
-            flags\ASSIGN_CONCAT => 'ASSIGN_CONCAT',
-            flags\ASSIGN_ADD => 'ASSIGN_ADD',
-            flags\ASSIGN_SUB => 'ASSIGN_SUB',
-            flags\ASSIGN_MUL => 'ASSIGN_MUL',
-            flags\ASSIGN_DIV => 'ASSIGN_DIV',
-            flags\ASSIGN_MOD => 'ASSIGN_MOD',
-            flags\ASSIGN_POW => 'ASSIGN_POW',
-            flags\ASSIGN_SHIFT_LEFT => 'ASSIGN_SHIFT_LEFT',
-            flags\ASSIGN_SHIFT_RIGHT => 'ASSIGN_SHIFT_RIGHT',
+            // flags\ASSIGN_BITWISE_OR => 'ASSIGN_BITWISE_OR',
+            // flags\ASSIGN_BITWISE_AND => 'ASSIGN_BITWISE_AND',
+            // flags\ASSIGN_BITWISE_XOR => 'ASSIGN_BITWISE_XOR',
+            // flags\ASSIGN_CONCAT => 'ASSIGN_CONCAT',
+            // flags\ASSIGN_ADD => 'ASSIGN_ADD',
+            // flags\ASSIGN_SUB => 'ASSIGN_SUB',
+            // flags\ASSIGN_MUL => 'ASSIGN_MUL',
+            // flags\ASSIGN_DIV => 'ASSIGN_DIV',
+            // flags\ASSIGN_MOD => 'ASSIGN_MOD',
+            // flags\ASSIGN_POW => 'ASSIGN_POW',
+            // flags\ASSIGN_SHIFT_LEFT => 'ASSIGN_SHIFT_LEFT',
+            // flags\ASSIGN_SHIFT_RIGHT => 'ASSIGN_SHIFT_RIGHT',
         ],
         ast\AST_MAGIC_CONST => [
             flags\MAGIC_LINE => 'MAGIC_LINE',
@@ -225,7 +225,7 @@ function ast_dump($ast, int $options = 0) : string {
     } else if ($ast === null) {
         return 'null';
     } else if (is_string($ast)) {
-        return "\"$ast\""; 
+        return "\"$ast\"";
     } else {
         return (string) $ast;
     }
